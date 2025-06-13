215fcd4f66f75b770f7183c2d2dca2168abd5334
lintangfahreza <lintangfahreza.lf@gmail.com>
2025-06-13 20:58:39 +0700

    database baru

--- sewa_alat.sql
+++ sewa_alat.sql
@@ -1,35 +1,137 @@
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nama VARCHAR(100),
-  email VARCHAR(100) UNIQUE,
-  password VARCHAR(255),
-  role ENUM('admin', 'user') DEFAULT 'user'
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 13, 2025 at 01:55 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.7
 
-CREATE TABLE alat_berat (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nama_alat VARCHAR(100),
-  jenis VARCHAR(50),
-  harga_per_hari INT,
-  deskripsi TEXT,
-  foto VARCHAR(255)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE transaksi (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  id_user INT,
-  id_alat INT,
-  tanggal_sewa DATE,
-  durasi INT,
-  total_biaya INT,
-  FOREIGN KEY (id_user) REFERENCES users(id),
-  FOREIGN KEY (id_alat) REFERENCES alat_berat(id)
-);
 
--- Tambahkan user admin default
-INSERT INTO users (nama, email, password, role) VALUES (
-  'admin',
-  'admin@mail.com',
-  'admin123',
-  'admin'
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sewa_alat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alat_berat`
+--
+
+CREATE TABLE `alat_berat` (
+  `id` int NOT NULL,
+  `nama_alat` varchar(100) DEFAULT NULL,
+  `jenis` varchar(50) DEFAULT NULL,
+  `harga_per_hari` int DEFAULT NULL,
+  `deskripsi` text,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int NOT NULL,
+  `id_user` int DEFAULT NULL,
+  `id_alat` int DEFAULT NULL,
+  `tanggal_sewa` date DEFAULT NULL,
+  `durasi` int DEFAULT NULL,
+  `total_biaya` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('admin','user') DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `email`, `password`, `role`) VALUES
+(2, 'Lintang', 'lintang@gmail.com', '$2y$10$KfHjvUl3w13nRfaV2gmuPOnSx6lNrjtPUOA.l2tVtcUOG9iTloj3q', 'admin'),
+(3, 'Divan', 'divan@gmail.com', '$2y$10$DSju4UXxyJB//6mmf7segevBFrwW0ZW4OV5tEQBFKy15Na1wuxsvG', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alat_berat`
+--
+ALTER TABLE `alat_berat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_alat` (`id_alat`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alat_berat`
+--
+ALTER TABLE `alat_berat`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_alat`) REFERENCES `alat_berat` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
